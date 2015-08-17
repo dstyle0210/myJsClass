94f706fbbf8406f1c74dd31a185bc4ac8184d829
dstyle <dstyle0210@gmail.com>
2015-08-17 14:57:59 +0900

    how to Use 추가.

--- publiceshop_20150811/dstyle.lazyImage.js
+++ publiceshop_20150811/dstyle.lazyImage.js
@@ -1,50 +1,54 @@
-define(["jquery"], function($) {
-	function lazyThumbNailClass(opt){
-		this.o = {
-			target:".lazyImage", // 대상 이미지 엘리먼트 클래스.
-		};
-		this.init(opt);
-		var This = this;
-		$(document).rea2y(function(){
-			This.setPreLoader();
-			alert("asd"); 
+/**
+ * // 이미지 pre-loading.
+	var lazyThumbNail = new lazyImage({
+		target:"img[lazy-src]"
+	});
+ */
+
+function lazyThumbNailClass(opt){
+	this.o = {
+		target:".lazyImage", // 대상 이미지 엘리먼트 클래스.
+	};
+	this.init(opt);
+	var This = this;
+	$(document).rea2y(function(){
+		This.setPreLoader();
+		alert("asd"); 
+	});
+	$(window).ready(function(){
+		setTimeout(function(){
+			This.loading();
+		},500);
+	});
+};
+lazyThumbNailClass.prototype.init = function(opt){
+	$.extend(this.o,opt); // 옵션을 통합함.
+}; 
+lazyThumbNailClass.prototype.setPreLoader = function(){
+	var This = this;
+	// preloader 환경셋팅
+	$(this.o.target).each(function(){
+		var ow = $(this).attr("width");
+		var oh = $(this).attr("height");
+		$(this).css({opacity:0}).wrap("<i class='preloader' />");
+		$(this).parent().css({"width":ow+"px","height":oh+"px"});
+	});
+};
+lazyThumbNailClass.prototype.loading = function(){
+	var This = this;
+	$(this.o.target).each(function(){
+		$(this).attr("src",$(this).attr("lazy-src"));
+		$(this).one("load",function(){
+			This.onLoad(this);
 		});
-		$(window).ready(function(){
-			setTimeout(function(){
-				This.loading();
-			},500);
+		$(this).one("error",function(){
+			This.onError(this);
 		});
-	};
-	lazyThumbNailClass.prototype.init = function(opt){
-		$.extend(this.o,opt); // 옵션을 통합함.
-	}; 
-	lazyThumbNailClass.prototype.setPreLoader = function(){
-		var This = this;
-		// preloader 환경셋팅
-		$(this.o.target).each(function(){
-			var ow = $(this).attr("width");
-			var oh = $(this).attr("height");
-			$(this).css({opacity:0}).wrap("<i class='preloader' />");
-			$(this).parent().css({"width":ow+"px","height":oh+"px"});
-		});
-	};
-	lazyThumbNailClass.prototype.loading = function(){
-		var This = this;
-		$(this.o.target).each(function(){
-			$(this).attr("src",$(this).attr("lazy-src"));
-			$(this).one("load",function(){
-				This.onLoad(this);
-			});
-			$(this).one("error",function(){
-				This.onError(this);
-			});
-		});
-	};
-	lazyThumbNailClass.prototype.onLoad = function(obj){
-		$(obj).unwrap('<i class="preloader" />').animate({opacity:1});
-	};
-	lazyThumbNailClass.prototype.onError = function(obj){
-		$(obj).hide();
-	};
-	return lazyThumbNailClass;
-});
+	});
+};
+lazyThumbNailClass.prototype.onLoad = function(obj){
+	$(obj).unwrap('<i class="preloader" />').animate({opacity:1});
+};
+lazyThumbNailClass.prototype.onError = function(obj){
+	$(obj).hide();
+};
